d77e7dce1aed7f9543d74c8dde96f8ad8f600f04
Junhyung Kim <kjun627@naver.com>
2025-11-20 15:12:47 +0900

    Update CmakeLists

---
+++ ch_4/threadLayout.cu
@@ -0,0 +1,28 @@
+#inlcude "cuda_runtime.h"
+#inlcude "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+__global__ void checkIndex(){
+    printf("treadIdx: (%d, %d, %d) blockIdx: (%d, %d, %d)
+    blockDim: (%d, %d, %d) gridDim: (%d, %d, %d,)\n",
+    threadIdx.x, threadIdx.y, threadIdx.z,
+    blockIdx.x, blockIdx.y, blockIdx.z,
+    blockDim.x, blockDim.y, blockDim.z,
+    gridDim.x, gridDim.y, gridDim.z);
+}
+
+int main(){
+    dim3 dimBlock(3,1,1);
+    dim3 dimGrid(2,1,1);
+
+    printf("dimGrid.x=%d dimGrid.y=%d dimGrid.z=%d\n",
+    dimGrid.x, dimGrid.y, dimGrid.z);
+    printf("dimBlock.x=%d dimBlock.y=%d dimBlock.z=%d\n",
+    dimBlock.x, dimBlock.y, dimBlock.z);
+
+    checkIndex<<<dimGrid, dimBlock>>>();
+    return 0;
+}
